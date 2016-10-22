d395f5e846d7b93ee3c4d4d6113888932926e97d
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2016-10-22 16:30:08 +0200

    init commit

---
+++ _config.php
@@ -0,0 +1,18 @@
+<?php
+
+define('INSITE_COMMON_DIR', basename(dirname(__FILE__)));
+
+define('BOWER_COMPONENTS_DIR', INSITE_COMMON_DIR."/components");
+define('BOOTSTRAP_DIR', BOWER_COMPONENTS_DIR . "/bootstrap/dist");
+define('JQUERY_DIR', BOWER_COMPONENTS_DIR . "/jquery/dist");
+define('JQUERY_FORM_DIR', BOWER_COMPONENTS_DIR . "/jquery-form");
+define('UNDERSCORE_DIR', BOWER_COMPONENTS_DIR . "/underscore");
+define('CHOSEN_PLUGIN_DIR', BOWER_COMPONENTS_DIR . "/chosen");
+define('MOMENT_DIR', BOWER_COMPONENTS_DIR . "/moment");
+define('CHOSEN_DIR', BOWER_COMPONENTS_DIR . "/chosen");
+define('MASONRY_DIR', BOWER_COMPONENTS_DIR . "/masonry");
+define('ISOTOPE_DIR', BOWER_COMPONENTS_DIR . "/isotope");
+define('DATA_TABLES_DIR', BOWER_COMPONENTS_DIR . "/datatables");
+define('JQUERY_UI_DIR', BOWER_COMPONENTS_DIR . "/jquery-ui");
+
+define("GOOGLE_MAP_API_KEY","AIzaSyCgaHPSWpnyc8774yWyQwxll-2ZLTgLyNA");
