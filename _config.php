4fd224679a6f6053d0cf15f41cb4fd80951210fa
Itayi Patrick Chito-voro <patrick@chitosystems.com>
2020-09-08 16:28:52 +0200

    batch code update - insite-apps-common-ecommerce

--- _config.php
+++ _config.php
@@ -1,6 +1,1 @@
 <?php
-
-define('INSITEAPPS_COMMON_ECOMMERCE_DIR', basename(dirname(__FILE__)));
-
-
-Config::inst()->update("ProductCategory", 'template_list', array("ProductItem" => "ProductItem"));
